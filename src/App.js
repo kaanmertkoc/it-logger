469314f34ba6d9a08e1076f30ad10179c34dcc57
kaanmertkoc <46743238+kaanmertkoc@users.noreply.github.com>
2021-03-29 01:13:17 +0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React, { Fragment, useEffect } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
 import './App.css';
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    // Init Materialize JS
+    M.AutoInit();
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <SearchBar />
+      <div className="container">
+        <Logs />
+      </div>
+    </Fragment>
   );
-}
+};
 
 export default App;
---
+++ src/components/layout/Preloader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Preloader = () => {
+  return (
+    <div className="progress blue ligthen-4">
+      <div className="indeterminate blue" />
+    </div>
+  );
+};
+export default Preloader;
